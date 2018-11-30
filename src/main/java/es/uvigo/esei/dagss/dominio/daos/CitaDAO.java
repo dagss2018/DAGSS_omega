97c7adf7b6b7a3a7caf109d6312ddc40191ce0d6
Guzhuu <bavalencia@esei.uvigo.es>
2018-11-30 09:52:13 +0100

    Fechas citas

--- src/main/java/es/uvigo/esei/dagss/dominio/daos/CitaDAO.java
+++ src/main/java/es/uvigo/esei/dagss/dominio/daos/CitaDAO.java
@@ -5,13 +5,21 @@
 package es.uvigo.esei.dagss.dominio.daos;
 
 import es.uvigo.esei.dagss.dominio.entidades.Cita;
+import java.sql.Date;
+import java.util.List;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
 
 
 @Stateless
 @LocalBean
 public class CitaDAO  extends GenericoDAO<Cita>{    
 
-    // Completar aqui
+    public List<Cita> getCitasPara(Date dia){
+        TypedQuery<Cita> q = em.createQuery("SELECT c FROM Cita AS c "
+                                              + "  WHERE c.fecha = :dia", Cita.class);
+        q.setParameter("dia", dia);
+        return q.getResultList();
+    }
 }
